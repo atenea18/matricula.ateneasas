adef57753c64c48d2466771a3dd6a28645755bb0
Jackson <jackson.palacios@correounivalle.edu.co>
2018-03-01 14:29:13 -0500

    Modificando migraciones

--- database/migrations/2018_03_01_142144_create_asignatures_table.php
+++ database/migrations/2018_03_01_142144_create_asignatures_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateCustomAsignaturesTable extends Migration
+class CreateAsignaturesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateCustomAsignaturesTable extends Migration
      */
     public function up()
     {
-        Schema::create('custom_asignatures', function (Blueprint $table) {
+        Schema::create('asignatures', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->string('code_custom_asignature')->unique();
-            $table->string('custom_name');
-            $table->integer('order');
+            $table->string('name');
+            $table->string('abbreviation');
             $table->timestamps();
         });
     }
@@ -29,6 +28,6 @@ class CreateCustomAsignaturesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('custom_asignatures');
+        Schema::dropIfExists('asignatures');
     }
 }
