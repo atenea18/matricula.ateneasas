c0d3163d01d71cdf356a09953fec7ed99c96e4db
Jackson <jackson.palacios@correounivalle.edu.co>
2018-03-08 17:47:27 -0500

    asignacion academica

--- resources/assets/js/components/areas-asignature/Manger.vue
+++ resources/assets/js/components/areas-asignature/Manger.vue
@@ -22,9 +22,22 @@
                             </div>
                         </div>
                     </tab>
-                    <!--<tab name="Pensum">
-                        Third tab content
-                    </tab>-->
+                    <tab name="Asignación">
+                        <div class="row">
+                            <div class="col-md-12">
+                                {{stringTest}}
+                                {{counter}}
+                                {{getDouble}}
+                                <div>
+                                    <button @click="increment">+</button>
+                                    <button @click="decrement">-</button>
+                                    <button @click="increments10">+10</button>
+                                    <button ></button>
+                                </div>
+                                <!--<add-group></add-group>-->
+                            </div>
+                        </div>
+                    </tab>
                 </tabs>
             </div>
         </div>
@@ -32,20 +45,44 @@
 </template>
 
 <script>
+    import {mapState, mapMutations, mapGetters } from 'vuex'
     import Add from './Add';
-    import AccordionAreas from './AccordionAreas';
+    import AddGroup from './AddGroup.vue';
     import TableCustom from "./Table";
 
     export default {
         name: "manger",
         components: {
             TableCustom,
-            Add, AccordionAreas
+            Add, AddGroup
+        },
+        computed: {
+            ...mapState(['counter']),
+            ...mapGetters(['getDouble']),
+
+            /*
+            double(){
+                return this.$store.getters.getDouble
+            },
+            */
+
+            stringTest() {
+                return "Si"
+            }
         },
         data() {
             return {
                 title: 'Adicionar Áreas',
             }
+        },
+        methods: {
+            ...mapMutations(['increment','decrement']),
+
+            increments10() {
+                this.$store.commit('increment',{
+                    number:10
+                })
+            },
         }
     }
 </script>
---
+++ resources/assets/js/components/partials/MultiSelect.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <label for="">Seleccionar {{type.name}}</label>
+        <multiselect 
+                v-model="value"
+                :options="data"
+                :multiple="isMultiple"
+                track-by="name"
+                :custom-label="customLabel">
+        </multiselect>
+    </div>
+</template>
+
+<script>
+    import Multiselect from 'vue-multiselect';
+    import {mapState, mapMutations, mapGetters } from 'vuex';
+
+    export default {
+        name: "multi-select",
+        props: {
+            data: {type: Array},
+            type: {type: Object}
+        },
+        computed:{
+            ...mapState(['asignatures']),
+        },
+        components: {Multiselect},
+        data(){
+            return{
+                value: [],
+                options: [],
+                isMultiple: true,
+            }
+        },
+        methods:{
+            customLabel(option) {
+                return `${option.name}`;
+            },
+        },
+        created(){
+
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
