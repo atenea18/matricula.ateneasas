77220832ffe5cb5254c77835111d554e4df3376d
Jackson <jackson.palacios@correounivalle.edu.co>
2018-04-26 17:27:17 -0500

    Se configura la relación de los desempeños con los subgrupos

--- app/Http/Controllers/Teacher/AsignatureController.php
+++ app/Http/Controllers/Teacher/AsignatureController.php
@@ -41,12 +41,14 @@ class AsignatureController extends ApiController
 
     public function asignaturesByGrade(request $request)
     {
+        $subject_type_id =  $request->isGroup == "true"?1:2;
 
         $pensum = DB::table('pensum')
             ->select('pensum.id as pensum_id', 'asignatures.id', 'asignatures.name', 'pensum.areas_id')
             ->join('asignatures', 'asignatures.id', '=', 'pensum.asignatures_id')
             ->where('pensum.grade_id', '=', $request->grade_id)
             ->where('pensum.areas_id', '=', $request->areas_id)
+            ->where('pensum.subjects_type_id', '=', $subject_type_id)
             ->where('pensum.institution_id', '=', $request->institution_id)
             ->get();
 
