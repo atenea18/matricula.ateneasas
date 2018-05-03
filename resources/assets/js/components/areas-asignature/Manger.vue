4e225c658dd8891975cc622d172db0bfbf43a233
Jackson <jackson.palacios@correounivalle.edu.co>
2018-05-03 01:03:24 -0500

    Se quita paginación a las tabla de asignación académica

--- resources/assets/js/components/areas-asignature/Manger.vue
+++ resources/assets/js/components/areas-asignature/Manger.vue
@@ -11,7 +11,7 @@
                             </div>
                         </div>
                     </tab>
-                    <tab name="Asignaci¨®n por Grupos">
+                    <tab name="AsignaciÃ³n por Grupos">
                         <div class="row">
                             <div class="col-md-12">
                                 <!--
