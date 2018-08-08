6b514a0de0aabd90ee0b5ffda369b39479c62afd
Jackson <jackson.palacios@correounivalle.edu.co>
2018-08-07 19:54:31 -0500

    Se agrega llave subjects al vector enrollments_by_groups

--- app/Helpers/Statistics/Consolidated/Dispatchers/PdfConsolidated.php
+++ app/Helpers/Statistics/Consolidated/Dispatchers/PdfConsolidated.php
@@ -17,7 +17,7 @@ use setasign\Fpdi\Fpdi;
 class PdfConsolidated extends AbstractConsolidated
 {
     private $params = null;
-    private $vectors = [];
+    private $enrollments_by_groups = [];
 
     public function __construct(ParamsStatistics $params)
     {
@@ -41,7 +41,8 @@ class PdfConsolidated extends AbstractConsolidated
             $this->createVectorGroupForPDF();
         }
 
-        return $this->createPdf();
+        //return $this->createPdf();
+        return $this->enrollments_by_groups;
 
     }
 
@@ -53,14 +54,17 @@ class PdfConsolidated extends AbstractConsolidated
         $information = (object)array(
             'group_id' => $this->params->group_object->id,
             'name' => $this->params->group_object->name,
-            'enrollments' => $this->vectorEnrollments);
-        array_push($this->vectors, $information);
+            'subjects' => $this->params->vectorSubjects,
+            'enrollments' => $this->vectorEnrollments
+        );
+
+        array_push($this->enrollments_by_groups, $information);
     }
 
     private function createPdf(){
         $fpdi = new Fpdi();
         $fpdi->SetLineWidth(.01);
-        foreach ($this->vectors as $key => $vector){
+        foreach ($this->enrollments_by_groups as $key => $vector){
             $fpdi->AddPage('Landscape', 'Letter',0);
             $fpdi->SetFont('Times', '', 10);
             $fpdi->SetTextColor(0, 0, 0);
@@ -69,7 +73,6 @@ class PdfConsolidated extends AbstractConsolidated
             }
 
         }
-
 
         $fpdi->Output('D', 'consolidated-'.$this->params->group_object->name.'.pdf',true);
     }
