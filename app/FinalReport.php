261d99ad3569e94c5751982bcc005ffc00e6e429
Jackson Jader <jpalacios@truora.com>
2018-12-03 19:50:03 -0500

    dddd

--- app/FinalReport.php
+++ app/FinalReport.php
@@ -13,8 +13,10 @@ class FinalReport extends Model
         return FinalReport::select(
             'enrollment.id as enrollment_id', 'final_report.id as final_report_id',
             'final_report.average', 'final_report.keep_going', 'final_report.description',
-            'final_report.rating', 'final_report.news_id')
+            'final_report.rating', 'final_report.news_id', 'news.name as news_name', 'news.id as news_other_id')
             ->join('enrollment', 'enrollment.id', '=', 'final_report.enrollment_id')
+            ->leftJoin('enrollment_news', 'enrollment_news.enrollment_id', '=', 'enrollment.id')
+            ->leftJoin('news', 'news.id', '=', 'enrollment_news.news_id')
             ->join('group_assignment', 'enrollment.id', '=', 'group_assignment.enrollment_id')
             ->where('group_assignment.group_id', '=', $group_id)
             ->get();
